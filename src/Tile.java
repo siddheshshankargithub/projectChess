93a0c620a7aa722e4e63b7f4f28432813fe26b8b
siddheshshankargithub <siddhesh.shankar@github.com>
2021-03-22 17:35:36 +0530

    Initial Tile class Commit

---
+++ src/Tile.java
@@ -0,0 +1,32 @@
+public abstract class Tile
+{
+    // A chess board has 64 tiles i.e. (8x8)
+    //to represent a tile number we use a tile coordinate:
+    int tileCoordinateNumber;
+    Tile(int tileCoordinateNumber)
+    {
+        this.tileCoordinateNumber = tileCoordinateNumber;
+    }
+    public abstract boolean isTileOccupancy();
+    public abstract Piece getPiece();
+
+    //sub-classes that represent an empty tile or an occupied tile.
+    public static final class EmptyTile extends Tile
+    {
+        EmptyTile(int coord)
+        {
+            super(coord);
+        }
+        @Override
+        public boolean isTileOccupancy()
+        {
+            return false;
+        }
+        public Piece getPiece()
+        {
+            // Since there is no piece on an empty tile hence an empty tile will return null
+            return null;
+        }
+
+    }
+}
